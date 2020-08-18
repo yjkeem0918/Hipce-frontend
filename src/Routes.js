7eac5e8ec5d690c29e39fd0a87546522722fa7cb
Joeun Lee <jjo.niixxx@gmail.com>
2020-08-18 17:07:15 +0900

    Add: HIPCE 초기 세팅 완료

---
+++ src/Routes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from '../src/Pages/Main/Main'
+import List from '../src/Pages/List/List'
+import Detail from '../src/Pages/Detail/Detail'
+import Sign from '../src/Pages/Sign/Sign'
+
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/Main" component={Main} />
+          <Route exact path="/List" component={List} />
+          <Route exact path="/Detail" component={Detail} />
+          <Route exact path="/Sign" component={Sign} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
