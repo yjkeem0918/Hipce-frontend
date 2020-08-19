e6ae62193eb0444748144126708d98bbd1343f8a
조은별 <ebcebeb@gmail.com>
2020-08-19 14:19:07 +0900

    Sign In 페이지 레이아웃 완료

--- src/Pages/Sign/Sign.js
+++ src/Pages/Sign/Sign.js
@@ -1,10 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Sign.scss";
 
 class Sign extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="Sign">
+        <div className="pageHeader">
+          <div className="pageTitle">Sign In</div>
+        </div>
+        <div className="formBox">
+          <form className="memberForm">
+            <div className="inputWrapper">
+              <input className="input" placeholder="아이디"></input>
+            </div>
+            <div className="inputWrapper">
+              <input className="input" placeholder="비밀번호"></input>
+            </div>
+            <div className="signInButtonBox">
+              <button className="signInButton">SIGN IN</button>
+              <div className="forgotAccount">
+                <div><a href="#">아이디 찾기</a></div>
+                <div><a href="#">비밀번호 찾기</a></div>
+              </div>
+            </div>
+            <div className="signInMenu">
+              <div className="descriptionBox">
+                <p class="description">아직 회원이 아니신가요?
+                  <br>
+                  </br>
+                가입 후 쿠폰 및 포인트 적립 등의 다양한 헤택을 받아보세요.
+                </p>
+              </div>
+              <button className="signUpButton">회원가입</button>
+              <div className="descriptionBox">
+                <p class="description">SNS 계정으로 가입하고 로그인 할 수 있습니다.</p>
+              </div>
+              <div className="socialSignIn">
+                <div className="facebook">
+                  <img src="Images/social-facebook.png"/>
+                </div>
+                <div className="naver">
+                  <img src="Images/social-naver.png"/>
+                </div>
+                <div className="kakaotalk">
+                  <img src="images/social-kakao.png"/>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Routes from './Routes';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
 
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
