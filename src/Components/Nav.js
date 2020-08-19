7c19c63daa7191efdf645b0da1b32a3c2fb97cf0
Joeun Lee <jjo.niixxx@gmail.com>
2020-08-19 14:44:15 +0900

    Add: Nav 레이아웃 완료

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Nav.scss";
 
 class Nav extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="Nav">
+        <div className="siteLogo">
+          <a href="/"></a>
+        </div>
+        <div className="siteMenu">
+          <ul>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Collection</a>
+            </li>
+            <li>
+              <a href="/">Shop</a>
+            </li>
+            <li>
+              <a href="/">Store</a>
+            </li>
+            <li>
+              <a href="/">Service</a>
+            </li>
+          </ul>
+        </div>
+        <div className="siteExtra">
+          <ul>
+            <li>
+              <a href="/"></a>
+            </li>
+            <li>
+              <a href="/"></a>
+            </li>
+            <li>
+              <a href="/"></a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
