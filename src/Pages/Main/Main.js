911420baed487231b07355b97fbcc5b76f151185
Joeun Lee <jjo.niixxx@gmail.com>
2020-08-21 19:29:11 +0900

    Refactor: 충돌 해결 - Main.js와 Routes.js에서 오류 해결

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import Nav from '../../Components/Nav'
+import React, { Component } from "react";
+import Footer from "../../Components/Footer";
 
 class Main extends Component {
   render() {
     return (
-      <div>
-        <Nav />
-      </div>
+      <>
+        <Footer />
+      </>
     );
   }
 }
