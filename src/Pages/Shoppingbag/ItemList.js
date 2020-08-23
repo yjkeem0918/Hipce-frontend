2e959ce1e7f20acd28efccaeca67b40c255c878c
cloudlee <cloudlee@MacBook-Pro-9.local>
2020-08-23 21:12:19 +0900

    Add: 장바구니 수량 추가 기능 완료

---
+++ src/Pages/Shoppingbag/ItemList.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+
+export default class ItemList extends Component {
+    constructor(props){
+        super(props)
+        this.handleCounting = this.handleCounting.bind(this);
+        this.state = {
+            pickedItem: props.pickedItem,
+            totalPrice: 0,
+        };
+    }
+
+    handleCounting=(item, inDecrement, e)=>{
+        if(e.target.name =="plusButton" && item.count == 5){
+            alert("최대주문수량 5개")
+            return
+        }
+        if(e.target.name === "plusButton"){
+            this.setState({
+                pickedItem: this.state.pickedItem.map(el=> el.name === item.name ? {...el, price: (el.price*(el.count+1))/(el.count), count: el.count+inDecrement}: el),
+            })
+        }
+        if(e.target.name == "minusButton" && item.count == 1){
+            return
+        }
+        if(e.target.name === "minusButton"){
+            this.setState({
+                pickedItem: this.state.pickedItem.map(el=> el.name === item.name ? {...el, price: parseInt((el.price*(el.count-1))/(el.count)), count: el.count+inDecrement}: el)
+            })
+        }
+        this.setState({totalPrice: this.state.pickedItem.map(el => el.price).reduce((a,b)=> a+b, 0)})
+    }
+
+    onFormSubmit =(e)=>{
+        e.preventDefault()
+        this.props.onSubmit(this.state.totalPrice)
+    }
+
+    deleteList=(item)=>{
+        this.setState({
+            pickedItem: this.state.pickedItem.filter(el => el.name !== item.name)
+        })
+    }
+    clearList = ()=>{
+        console.log(this.state.pickItem)
+        this.setState({pickItem:''})
+        console.log(this.state.pickItem)
+    }
+
+    render() {
+        console.log("state",this.state.pickedItem)
+        console.log("props",this.props.pickedItem)
+        return (
+            <div className = "ItemList">
+                {this.state.pickedItem.map((item, index)=>{
+                    return <td key = {index} className = "pickItem">
+                    <div className = "pickItemPicture">
+                        <input type = "checkbox" id = {`checkbox${index}`}/>
+                        <label for = {`checkbox${index}`}></label>
+                        <img alt = "productImage" src = {item.img}/>
+                        <span className = "productName">{item.name}</span>
+                    </div>
+                    <div className = "pickItemCount">
+                        <form className = "count" onSubmit ={this.onFormSubmit}>
+                            <button className = "countButton" name = "minusButton" onClick = {(e)=>this.handleCounting(item, -1, e)}> - </button>
+                            <span>{item.count}</span>
+                            <button className = "countButton" name = "plusButton" onClick = {(e)=>this.handleCounting(item, +1, e)}> + </button>
+                        </form>
+                        <div className = "eachPrice">{item.price}원</div>
+                        <button onClick = {()=>this.deleteList(item)}>X</button>
+                    </div>
+                </td>
+                })}
+            </div>
+        )
+    }
+
+}
--- src/Pages/Shoppingbag/Shoppingbag.js
+++ src/Pages/Shoppingbag/Shoppingbag.js
@@ -1,36 +1,60 @@
 import React, { Component } from 'react'
 import {Link} from'react-router-dom'
+import Nav from '../../Components/Nav'
+import Footer from '../../Components/Footer'
 import '../../Styles/common.scss'
 import './Shoppingbag.scss'
 import '../../Styles/reset.scss'
+import './ItemList'
+import ItemList from './ItemList'
 
 export default class Shoppingbag extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            countNumber: {},
+            pickItem : [...pickedItem],
+            totalPrice: 0,
+            realtotalPrice: null,
+            shippingFee: 2500,
+        }
+    }
+    
+    componentDidMount(){
+        // fetch.("URL")
+        // .then(res => res.json())
+        // .then(res =>this.setState({pickItem: res.map(el => el.sth)}))
+        this.setFirstPrice()
+    }
+
+    setFirstPrice = ()=>{
+        this.setState({realtotalPrice: this.state.pickItem.map(el => el.price).reduce((a,b)=> a+b, 0)})
+    }
+
+    onSearchSubmit=(totalPrice)=>{
+        this.setState({realtotalPrice: totalPrice})
+        if(totalPrice >= 50000){
+            this.setState({shippingFee: 0})
+        }else if(totalPrice < 50000){
+            this.setState({shippingFee: 5000})
+        }
+    }
+    clearList = ()=>{
+        console.log(this.state.pickItem)
+        this.setState({pickItem: <span>what</span>})
+        console.log(this.state.pickItem)
+    }
+
     render() {
         return (
             <div className = "Shoppingbag">
+                <Nav />
                 <span className = "ShoppingbagTitle">Shopping Bag</span>
                 <section className = "OrderdList">
                     <span className = "ShoppingbagEmpty">장바구니에 담으신 상품이 없습니다.</span>
                     <table className = "OrderdProductList">
-                        <tr>
-                            <td className = "pickItem">
-                                <div className = "pickItemPicture">
-                                    <input type = "checkbox" id = "checkbox"/>
-                                    <label for = "checkbox"></label>
-                                    <img alt = "productImage" src = "https://hince.co.kr/web/product/tiny/20200617/b4b788b34fdd1c711ee75bf74a8096a6.jpg" />
-                                    <span className = "productName">무드인핸서 리퀴드 마뜨 오운 스킨</span>
-                                </div>
-                                <div className = "pickItemCount">
-                                    <div className = "count">
-                                        <Link to = "/List">-</Link>
-                                        <span>1</span>
-                                        <Link to = "/List">+</Link>
-                                    </div>
-                                    <div className = "eachPrice">19,000원</div>
-                                    <button>X</button>
-                                </div>
-                            </td>
-                        </tr>
+                        <ItemList pickedItem = {this.state.pickItem} totalPrice = {this.state.totalPrice} onSubmit ={this.onSearchSubmit} clearList = {this.clearList}/>
                     </table>
                     </section>
                 <section className = "ShoppingbagFooter">
@@ -38,25 +62,25 @@ export default class Shoppingbag extends Component {
                         <div>
                             <span>5만원 이상 결제 시 무료로 배송 받을 수 있습니다.</span>
                             <div>
-                                <Link to = "/List">선택한 상품 삭제하기</Link>
-                                <Link to = "/List">장바구니 비우기</Link>
+                                <span >선택한 상품 삭제하기</span>
+                                <span onClick = {this.clearList}>장바구니 비우기</span>
                             </div>
                         </div>
                     </div>     
                     <div className = "PriceCalculatedWrapper">
-                        <div className = "PriceCalculated">
+                        <div className = "PriceCalculated" onChange ={(e)=>this.cutValue(e)}>
                             <ul>
                                 <li>
                                     <span>주문금액</span>
-                                    <span className = "CalculatedPrice">5,000원</span>
+                                    <span className = "CalculatedPrice" >{this.state.realtotalPrice}원</span>
                                 </li>
                                 <li>
                                     <span>배송비</span>
-                                    <span className = "ShippingFee">5,000원</span>
+                                    <span className = "ShippingFee">{this.state.shippingFee}원</span>
                                 </li>
                                 <li className = "sum">
                                     <span >합계</span>
-                                    <span className = "ShippingFee">10,000원</span>
+                                    <span className = "ShippingFee" >{this.state.realtotalPrice+this.state.shippingFee}원</span>
                                 </li>
                             </ul>
                         </div>
@@ -69,7 +93,27 @@ export default class Shoppingbag extends Component {
                         </div>
                     </div>
                 </section>
+                <Footer />
             </div>
         )
     }
+
 }
+//sampleData
+const pickedItem = [
+    {img: "https://hince.co.kr/web/product/tiny/20200617/b4b788b34fdd1c711ee75bf74a8096a6.jpg",
+    name:"무드인핸서 리퀴드 마뜨 오운 스킨",
+    price:19000,
+    count:1,
+    },
+    {img: "https://hince.co.kr/web/product/tiny/20200617/b4b788b34fdd1c711ee75bf74a8096a6.jpg",
+    name:"무드 무드무드 리퀴드 마뜨 오운 스킨",
+    price:15000,
+    count:1,
+    },
+    {img: "https://hince.co.kr/web/product/tiny/20200617/b4b788b34fdd1c711ee75bf74a8096a6.jpg",
+    name:"무드 무드무드 와라라 마뜨 오운 스킨",
+    price:15000,
+    count:1,
+    }
+]
