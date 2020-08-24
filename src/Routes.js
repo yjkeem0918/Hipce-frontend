3f50e44e3942773c82ba555e28a72144b25d66d9
조은별 <ebcebeb@gmail.com>
2020-08-24 14:55:01 +0900

    중간발표용

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
-import Main from '../src/Pages/Main/Main'
-import List from '../src/Pages/List/List'
-import Detail from '../src/Pages/Detail/Detail'
-import Sign from '../src/Pages/Sign/Sign'
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Main from "../src/Pages/Main/Main";
+import List from "../src/Pages/List/List";
+import Detail from "../src/Pages/Detail/Detail";
+import Sign from "../src/Pages/Sign/Sign";
+import SignUp from "../src/Pages/Sign/SignUp";
 
 class Routes extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class Routes extends React.Component {
           <Route exact path="/List" component={List} />
           <Route exact path="/Detail" component={Detail} />
           <Route exact path="/Sign" component={Sign} />
+          <Route exact path="/SignUp" component={SignUp} />
         </Switch>
       </Router>
     );
